c67783c55ae4c3a38d19b7c71df63906a94dddc8
zray <1872710105@qq.com>
2022-06-21 18:06:21 +0800

    新增swagger

--- trage/src/main/java/com/zray/cam/dim/controller/UserController.java
+++ trage/src/main/java/com/zray/cam/dim/controller/UserController.java
@@ -2,12 +2,14 @@ package com.zray.cam.dim.controller;
 
 import com.zray.cam.dim.domain.User;
 import com.zray.cam.dim.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/dim/user")
+@Api(tags = "用户")
 public class UserController {
 
     @Autowired
